aef31cf803ea58cb29f11d9c5446114b8360c887
YumYumNyang <khw121699@gmail.com>
2020-07-09 14:24:58 +0900

    Revert "show selected menu list"
    This reverts commit 75550aee

--- Menu_app/app/src/main/java/org/techtown/menu_app/addMenu.java
+++ Menu_app/app/src/main/java/org/techtown/menu_app/addMenu.java
@@ -68,7 +68,7 @@ public class addMenu extends AppCompatActivity {
                     Toast.makeText(addMenu.this, menu_name+" 추가 완료", Toast.LENGTH_SHORT).show();
                     break;
                 }
-                ((Setting)Setting.CONTEXT).onResume();
+                //((Setting)Setting.CONTEXT).onResume();
                 finish();
             }
         });
